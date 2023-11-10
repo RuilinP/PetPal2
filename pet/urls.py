b9306b902b6431e4ccb14f59d22ea4c00cde506a
Ruilin P's ArchLinux <ruilin.peng@mail.utoronto.ca>
2023-11-10 13:29:41 -0500

    merged the search page

--- pet/urls.py
+++ pet/urls.py
@@ -4,6 +4,5 @@ from .views import PetCreateView, PetRetrieveUpdateDestroyView, PetListView, Pet
 urlpatterns = [
     path('pet/', PetCreateView.as_view(), name='pet-create'),
     path('pet/<int:pk>', PetRetrieveUpdateDestroyView.as_view(), name='pet-detail'),
-    path('pets/', PetListView.as_view(), name='pet-list'),
-    path('pets/search/', PetListSearch.as_view(), name='pet-list'),
+    path('pets/', PetListSearch.as_view(), name='pet-list'),
 ]
