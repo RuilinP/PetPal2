455f251a98a6c835bd91dd8a7dad0620988e08d0
Ruilin P's ArchLinux <ruilin.peng@mail.utoronto.ca>
2023-11-10 13:18:32 -0500

    filter disabled at listing page enabled at search page, multiple filter enabled

--- pet/urls.py
+++ pet/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PetCreateView, PetRetrieveUpdateDestroyView, PetListView
+from .views import PetCreateView, PetRetrieveUpdateDestroyView, PetListView, PetListSearch
 
 urlpatterns = [
-    path('pet', PetCreateView.as_view(), name='pet-create'),
+    path('pet/', PetCreateView.as_view(), name='pet-create'),
     path('pet/<int:pk>', PetRetrieveUpdateDestroyView.as_view(), name='pet-detail'),
-    path('pets', PetListView.as_view(), name='pet-list'),
+    path('pets/', PetListView.as_view(), name='pet-list'),
+    path('pets/search/', PetListSearch.as_view(), name='pet-list'),
 ]
