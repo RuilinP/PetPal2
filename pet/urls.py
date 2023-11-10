70bade48e53e6f8f3e6c913bf508fe46a2be3728
Ruilin P's ArchLinux <ruilin.peng@mail.utoronto.ca>
2023-11-09 19:39:56 -0500

    updated run.sh setup.sh

--- pet/urls.py
+++ pet/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PetCreateView, PetRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('pets/create', PetCreateView.as_view(), name='pet-create'),
+    path('pets/', PetCreateView.as_view(), name='pet-create'),
     path('pets/<int:pk>/', PetRetrieveUpdateDestroyView.as_view(), name='pet-detail'),
 ]
