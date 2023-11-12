74f523c0326c3b74444d1abcf281781dfc2405f3
Cao Jennifer Qian Tung <caojenn3@teach.cs.toronto.edu>
2023-11-12 11:23:30 -0500

    Modify accounts models + add accounts views

--- petpal/urls.py
+++ petpal/urls.py
@@ -20,8 +20,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('api/token/refresher/', TokenRefreshView.as_view(), name='token-refresh'),
     path('accounts/', include('accounts.urls')),
     path('', include('pet.urls')),
 ]
