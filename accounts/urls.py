b1e88f236d9940384ed2eaf3f6a3ca31812b6690
Cao Jennifer Qian Tung <caojenn3@teach.cs.toronto.edu>
2023-11-11 23:34:44 -0500

    Fix issues with making migrations

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import ShelterCreateRetrieveView, ShelterRetrieveUpdateView, SeekerCreateView, SeekerRetrieveUpdateView
+# from .views import ShelterCreateRetrieveView, ShelterRetrieveUpdateView, SeekerCreateView, SeekerRetrieveUpdateView
 
 urlpatterns = [
-    path('shelters/', ShelterCreateRetrieveView.as_view(), name='shelter-create'),
-    path('shelters/<int:pk>/', ShelterRetrieveUpdateView.as_view(), name='shelter-update'),
-    path('seekers/', SeekerCreateView.as_view(), name='seeker-create'),
-    path('seekers/<int:pk>/', SeekerRetrieveUpdateView.as_view(), name='seeker-update'),
+    # path('shelters/', ShelterCreateRetrieveView.as_view(), name='shelter-create'),
+    # path('shelters/<int:pk>/', ShelterRetrieveUpdateView.as_view(), name='shelter-update'),
+    # path('seekers/', SeekerCreateView.as_view(), name='seeker-create'),
+    # path('seekers/<int:pk>/', SeekerRetrieveUpdateView.as_view(), name='seeker-update'),
 ]
