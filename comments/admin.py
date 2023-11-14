0244a00dfbf59cf48e3e91b222af41c6e58a2196
Star <43018428+Starliyiyu@users.noreply.github.com>
2023-11-13 19:56:37 -0500

    fix error

--- comments/admin.py
+++ comments/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from .models import Comment, Reply, Shelter, Application
-
-admin.site.register(Comment)
-admin.site.register(Reply)
-admin.site.register(Shelter)
-admin.site.register(Application)
