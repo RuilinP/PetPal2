24c24ce1dfe943da5aae9b40010fbb0d58ec9f62
Cao Jennifer Qian Tung <caojenn3@teach.cs.toronto.edu>
2023-11-18 16:43:32 -0500

    Add post and get methods for application

--- application/serializers.py
+++ application/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import Application
 
 class ApplicationSerializer(serializers.ModelSerializer):
+    seeker = serializers.PrimaryKeyRelatedField(read_only=True)
+    shelter = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Application
         fields = ['id', 'seeker', 'shelter', 'pet', 'status', 'message', 'created_at', 'updated_at']
