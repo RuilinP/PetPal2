7251583b7146898cc5c58673a72cf427b52da026
Star <43018428+Starliyiyu@users.noreply.github.com>
2023-11-17 19:09:09 -0500

    app added: applications, applications.models.py finished, a simple serializer added too

---
+++ application/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from accounts.models import Seeker, Shelter
+from pet.models import Pet
+
+class Application(models.Model):
+    seeker = models.ForeignKey(Seeker, on_delete=models.CASCADE, related_name='applications')
+    shelter = models.ForeignKey(Shelter, on_delete=models.CASCADE, related_name='applications')
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE, related_name='applications')
+    
+    STATUS_CHOICES = [ # seeker actions allowed: pending, accepted, withdrawn; shelter actions allowed: pending, accepted, denied
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('denied', 'Denied'),
+        ('withdrawn', 'Withdrawn')
+    ]
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    message = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at', '-updated_at']
+
+
+ # to minimize redundancy, fields such as the seeker's address, phone number,.etc are not included in the model.
+ # These can be accessed through the Seeker object stored in seeker field
+ # lmk if you think it is necessary to have those fields in the model
