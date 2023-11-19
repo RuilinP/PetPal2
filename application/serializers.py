f50c52a9eeea4d526e9c5d39ee2c6cb337715c02
Ruilin P's ArchLinux <ruilin.peng@mail.utoronto.ca>
2023-11-18 21:10:02 -0500

    added pet to application serializer

--- application/serializers.py
+++ application/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import Application
 from accounts.models import Shelter, Seeker 
+from pet.models import Pet
 
 class ApplicationSerializer(serializers.ModelSerializer):
     seeker = serializers.PrimaryKeyRelatedField(queryset=Seeker.objects.all())
     shelter = serializers.PrimaryKeyRelatedField(queryset=Shelter.objects.all())
+    pet = serializers.PrimaryKeyRelatedField(queryset=Pet.objects.all())
 
     class Meta:
         model = Application
