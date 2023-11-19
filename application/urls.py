4b388b016c1f7090406b7b3add38a7c95591e995
Ruilin P's ArchLinux <ruilin.peng@mail.utoronto.ca>
2023-11-18 23:12:55 -0500

    listview of application

--- application/urls.py
+++ application/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ApplicationCreateView, ApplicationRetrieveView
+from .views import ApplicationCreateView, ApplicationRetrieveView, ApplicationListAPIView
 
 urlpatterns = [
-    path('applications/', ApplicationCreateView.as_view(), name='applications-create'),
+    path('application/', ApplicationCreateView.as_view(), name='applications-create'),
     path('applications/<int:pk>/', ApplicationRetrieveView.as_view(), name='applications-retrieve'),
+    path('applications/', ApplicationListAPIView.as_view(), name='applications-list'),
 ]
--- application/views.py
+++ application/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated, BasePermission
 from .serializers import ApplicationSerializer
 from .models import Application
+from rest_framework.filters import OrderingFilter
+from rest_framework.pagination import PageNumberPagination
+
 
 
 class ApplicationCreateView(CreateAPIView):
@@ -16,8 +19,19 @@ class ApplicationRetrievePermission(BasePermission):
         
         application = get_object_or_404(Application, id=view.kwargs['pk'])
         # Check that user requesting is the application's seeker or shelter
-        print(application.seeker.id)
-        return request.user.id == application.seeker.id or request.user.id == application.shelter
+        return request.user.id == application.seeker.id or request.user.id == application.shelter.id
+
+class ApplicationEditPermission(BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        print(request.user.id)
+        print(view)
+        application = Application.objects.filter(shelter=request.user)
+        if not application:
+            return False
+        return True
+
 
 class ApplicationRetrieveView(RetrieveAPIView):
     serializer_class = ApplicationSerializer
@@ -25,3 +39,19 @@ class ApplicationRetrieveView(RetrieveAPIView):
 
     def get_object(self):
         return get_object_or_404(Application, id=self.kwargs['pk'])
+
+class ApplicationListAPIView(ListAPIView):
+    serializer_class = ApplicationSerializer
+    permission_classes = [ApplicationEditPermission]
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['created_at', 'updated_at']
+    pagination_class = PageNumberPagination
+
+    def get_queryset(self):
+        status = self.request.query_params.get('status') 
+        queryset = Application.objects.filter(shelter=self.request.user)
+        if status:
+            queryset = queryset.filter(status=status)
+        queryset = queryset.order_by('-created_at', '-updated_at')
+
+        return queryset
